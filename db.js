3823e11f936b96339755c384e97578bb8b1b8942
alexander <alexander.coroneldiaz.1@gmail.com>
2024-10-07 18:48:37 -0600

    Mensaje descriptivo sobre los cambios realizados

--- db.js
+++ db.js
@@ -1,12 +1,24 @@
-// db.js
+// // db.js
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'optimizacion_documentos',
+//   password: 'Admin',
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
+require('dotenv').config(); // Cargar las variables de entorno desde el archivo .env
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'optimizacion_documentos',
-  password: 'Admin',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL, // Usar la URL de la base de datos de Render
+  ssl: {
+    rejectUnauthorized: false, // Esto es necesario para las conexiones SSL en Render
+  },
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config(); // Cargar las variables de entorno desde el archivo .env
 
 // Importar rutas de usuarios
 const usuariosRoutes = require('./conexiones/usuarios');
-const RolesRoutes = require('./conexiones/roles')
-const DocumentosRoutes = require('./conexiones/documentos')
-const Routes = require('./conexiones/procesos_ocr')
-const ResumenesRoutes = require('./conexiones/resumenes')
-const EtiquetasRoutes = require('./conexiones/etiquetas')
-const LogsRoutes = require('./conexiones/logs')
-const ComentariosRoutes = require('./conexiones/comentarios_documentos')
-const Permisos = require('./conexiones/permisos_tablas')
-
+const RolesRoutes = require('./conexiones/roles');
+const DocumentosRoutes = require('./conexiones/documentos');
+const Routes = require('./conexiones/procesos_ocr');
+const ResumenesRoutes = require('./conexiones/resumenes');
+const EtiquetasRoutes = require('./conexiones/etiquetas');
+const LogsRoutes = require('./conexiones/logs');
+const ComentariosRoutes = require('./conexiones/comentarios_documentos');
+const Permisos = require('./conexiones/permisos_tablas');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -28,8 +28,6 @@ app.use('/etiquetas', EtiquetasRoutes);
 app.use('/logs', LogsRoutes);
 app.use('/comentarios_documentos', ComentariosRoutes);
 app.use('/permisos_tablas', Permisos);
-
-
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
