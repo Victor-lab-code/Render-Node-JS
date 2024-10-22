f5c65aa25e6b520d8a2fde3bd3d27c13e31af98b
alexander <alexander.coroneldiaz.1@gmail.com>
2024-10-21 19:21:11 -0600

    Se busca quirtar el scan en viewe

--- conexiones/middlewares/verificarRol.js
+++ conexiones/middlewares/verificarRol.js
@@ -1,23 +1,29 @@
 const jwt = require('jsonwebtoken');
 
+// Middleware para verificar roles
 function verificarRol(rolesPermitidos) {
-  return (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-      return res.status(403).json({ error: "Acceso denegado" });
-    }
-
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET || 'mi_secreto_super_seguro');
-      if (!rolesPermitidos.includes(decoded.rol)) {
-        return res.status(403).json({ error: "No tienes permisos para realizar esta acción" });
+    return (req, res, next) => {
+      const token = req.headers.authorization?.split(" ")[1];
+      if (!token) {
+        return res.status(403).json({ error: "Acceso denegado" });
       }
-      req.user = decoded; // Guardar el token decodificado para usarlo más adelante
-      next();
-    } catch (err) {
-      res.status(401).json({ error: "Token inválido" });
-    }
-  };
-}
+  
+      try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        //const decoded = jwt.verify(token, process.env.JWT_SECRET || 'mi_secreto_super_seguro');
+        req.user = decoded; // Guardar la información del usuario en req.user
+  
+        if (!rolesPermitidos.includes(decoded.rol)) {
+          return res.status(403).json({ error: "No tienes permisos para realizar esta acción" });
+        }
+  
+        next();
+      } catch (err) {
+        res.status(401).json({ error: "Token inválido" });
+      }
+    };
+  }
+  
 
 module.exports = verificarRol;
