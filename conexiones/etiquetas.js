b86fb8a605fa98e39f964f6a485968dc3929d2a5
Victor-lab-code <vgsc1asop.cbtis163@gmail.com>
2024-11-11 22:24:46 -0600

    Se agrega funcion de etiquetas

--- conexiones/etiquetas.js
+++ conexiones/etiquetas.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const pool = require('../db'); // Asegúrate de tener configurada la conexión a tu base de datos
+const pool = require('../db'); // Configuración de conexión a la base de datos
 const router = express.Router(); // Define el router para manejar las rutas
 
-// Obtener todas las etiquetas
+// Obtener todas las etiquetas de un usuario
 router.get('/', async (req, res) => {
+  const { userId } = req.query; // Obtenemos el userId desde los parámetros de consulta
+
   try {
-    const result = await pool.query('SELECT * FROM etiquetas');
+    if (!userId) {
+      return res.status(400).send('El userId es requerido');
+    }
+
+    // Consulta para obtener las etiquetas del usuario especificado
+    const result = await pool.query('SELECT * FROM etiquetas WHERE usuario_id = $1', [userId]);
     res.json(result.rows);
   } catch (err) {
     console.error('Error al obtener las etiquetas:', err);
@@ -15,19 +22,21 @@ router.get('/', async (req, res) => {
 
 // Agregar una nueva etiqueta
 router.post('/', async (req, res) => {
-  const { documento_id, etiqueta } = req.body;
+  const { documento_id, etiqueta, color, usuario_id } = req.body;
   
   // Log para verificar los datos recibidos
-  console.log('Datos recibidos:', { documento_id, etiqueta });
+  console.log('Datos recibidos:', { documento_id, etiqueta, color, usuario_id });
   
   try {
-    if (!documento_id || !etiqueta) {
-      return res.status(400).send('El documento_id y la etiqueta son requeridos');
+    // Validar que todos los datos requeridos estén presentes
+    if (!documento_id || !etiqueta || !color || !usuario_id) {
+      return res.status(400).send('documento_id, etiqueta, color y usuario_id son requeridos');
     }
 
+    // Insertar la nueva etiqueta en la base de datos
     const result = await pool.query(
-      'INSERT INTO etiquetas (documento_id, etiqueta, fecha_etiqueta) VALUES ($1, $2, NOW()) RETURNING *',
-      [documento_id, etiqueta]
+      'INSERT INTO etiquetas (documento_id, etiqueta, color, fecha_etiqueta, usuario_id) VALUES ($1, $2, $3, NOW(), $4) RETURNING *',
+      [documento_id, etiqueta, color, usuario_id]
     );
 
     res.status(201).json(result.rows[0]);
@@ -41,7 +50,8 @@ router.post('/', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query('DELETE FROM etiquetas WHERE id = $1', [id]);
+    // Ejecutar la consulta de eliminación
+    await pool.query('DELETE FROM etiquetas WHERE id = $1', [id]);
     res.status(200).send('Etiqueta eliminada');
   } catch (err) {
     console.error('Error al eliminar etiqueta:', err);
